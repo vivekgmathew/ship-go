5e78b3d7d702a9eaab1344a784c1568963bee685
vivekmathew <vivekgmathew@gmail.com>
2023-08-19 20:42:54 -0500

    test

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	addr := ":8080"                                     ❶
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/hello",
+		func(w http.ResponseWriter, r *http.Request) {
+			❷
+			enc := json.NewEncoder(w)
+			w.Header().
+				Set("Content-Type",
+					"application/json; charset=utf-8")
+			resp := Resp{
+				Language:    "English",
+				Translation: "Hello",
+			}
+			if err := enc.Encode(resp); err != nil {
+				panic(fmt.Sprintf("Error %s", err.Error()))
+			}
+		})
+
+	log.Fatal(http.ListenAndServe(addr, mux))
+}
+
+type Resp struct {
+	Language    string `json:"language"`
+	Translation string `json:"translation"`
+}
